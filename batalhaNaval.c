70967e8d0387bafe57ba98120dbc9fbe46c92ffc
Secret Index <caio.smelo04@gmail.com>
2025-09-12 00:43:38 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -20,6 +20,79 @@ int main() {
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
     // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
+  int board[10][10] = {0};
+
+  // Add horizontal ship into the board - number 1
+  for (int i = 0; i < 3; i++)
+  {
+    board[2][i] = 1;
+  }
+
+  // Add vertical ship into the board - number 2
+  for (int i = 4; i < 7; i++)
+  {
+    board[i][6] = 2;
+  }
+
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < 10; j++)
+    {
+      printf("%d ", board[i][j]);
+    }
+    printf("\n");
+  }
+
+  printf("\n");
+  printf("Adventure Level");
+  printf("\n");
+
+  // Ship one in the board - 1
+  // Ship two in the board - 2
+
+  // -------------------------
+  // Adventure Level
+  // -------------------------
+
+  int advBoard[10][10] = {0};
+
+  // First ship
+  for (int i = 1; i < 4; i++)
+  {
+    advBoard[1][i] = 1;
+  }
+
+  // Second ship
+  for (int i = 5; i < 8; i++)
+  {
+    advBoard[4][i] = 2;
+  }
+
+  // Third ship
+  for (int i = 7; i < 10; i++)
+  {
+    advBoard[i][3] = 3;
+  }
+
+  // Fourth ship
+  for (int i = 1; i < 4; i++)
+  {
+    advBoard[i][8] = 4;
+  }
+
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < 10; j++)
+    {
+      printf("%d ", advBoard[i][j]);
+    }
+    printf("\n");
+  }
+
+  // -------------------------
+  // Master Level
+  // -------------------------
+    
     // Exemplos de exibição das habilidades:
     // Exemplo para habilidade em cone:
     // 0 0 1 0 0
